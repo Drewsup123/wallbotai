93c672bb69ee25cda686981ab895727e8ceea499
Drew <drewsup123c@gmail.com>
2023-04-23 09:19:47 -0600

    More boilerplate, added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Canvas from "./canvas";
+import Customizer from "./pages/Customizer.page";
+import Home from "./pages/Home.page";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Home />
+            <Customizer />
+            <Canvas />
+        </div>
+    );
 }
 
 export default App;
